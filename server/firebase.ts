dadb02c9153aa5910839e5d1b25924e843dbe589
Michelle Li <michelleyli123@gmail.com>
2023-11-08 04:28:07 -0500

    template

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,2 +1,6 @@
+// @ts-nocheck
 import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
 import { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore";
+import serviceAccount from "./service_account.json";
+
+export { db };
---
+++ server/people.controller.ts
@@ -0,0 +1,27 @@
+// @ts-nocheck
+
+import { db } from "./firebase";
+import { Person } from "../common/types";
+
+export const addPerson = async (netid: string, person: Person) => {
+
+};
+
+export const getPeople = async () => {
+
+};
+
+export const getPerson = async (netid: string) => {
+
+};
+
+export const getCertainAge = async (age: number) => {
+
+};
+
+export const updateAge = async (netid: string, age: number) => {
+
+};
+
+export const deletePerson = async (netid: string) => {
+};
